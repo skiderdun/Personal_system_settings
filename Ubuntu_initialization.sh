93b99b47ea9c84b33ca66b83c3c23ab5b8fc196f
skiderdun <skiderdun@gmail.com>
2022-10-14 14:39:42 -0500

    new Ubuntu init file

---
+++ Ubuntu_initialization.sh
@@ -0,0 +1,27 @@
+#!/bin/bash/sh
+
+echo "We'll setup NeoVim first"
+
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
+cd ~
+mkdir .config
+
+cp Personal_system_settings/.bashrc ~/
+
+cp Personal_system_settings/nvim .config/
+
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
+
+nvim --headless -S ".config/nvim/plugins.lua" -c "qa!"
+nvim --headless -c "PackerSync" -c "qa!"
+nvim --headless -S ".config/nvim/init.lua" -c "qa!"
+
+echo "NeoVim setup complete"
+
