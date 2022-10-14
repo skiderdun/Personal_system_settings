61a8b1acc076a1160892b92a32ada1afbc1828e9
skiderdun <skiderdun@gmail.com>
2022-10-14 14:02:06 -0500

    New working init for nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,12 @@
+
+function script_path()
+	local str = debug.getinfo(2, "S").source:sub(2)
+	return str:match("(.*/)")
+end
+
+package.path = string.format("%s;%s?.lua", package.path, script_path())
+
+
 require('plugins')
 
 
