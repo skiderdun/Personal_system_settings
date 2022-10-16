29d0b458c7aae63bdf9a105f9f6c6a298d83ed88
Skiderdun <skiderdun@gmail.com>
2022-10-16 16:10:49 -0500

    new init file

--- Ubuntu_initialization.sh
+++ Ubuntu_initialization.sh
@@ -5,12 +5,12 @@ echo "We'll setup NeoVim first"
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install neovim
-cd ~
-mkdir .config
+cd ~/
+mkdir ~/.config/
 
-cp Personal_system_settings/.bashrc ~/
+cp -r ~/Personal_system_settings/.bashrc ~/
 
-cp Personal_system_settings/nvim .config/
+cp -r ~/Personal_system_settings/nvim ~/.config/
 
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --config vi
@@ -19,9 +19,18 @@ sudo update-alternatives --config vim
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 sudo update-alternatives --config editor
 
-nvim --headless -S ".config/nvim/plugins.lua" -c "qa!"
+nvim --headless -S "~/.config/nvim/plugins.lua" -c "qa!"
 nvim --headless -c "PackerSync" -c "qa!"
-nvim --headless -S ".config/nvim/init.lua" -c "qa!"
+nvim --headless -S "~/.config/nvim/init.lua" -c "qa!"
 
 echo "NeoVim setup complete"
 
+echo "\n Now We'll make sure you have your compilers sorted"
+
+sudo apt install gcc
+
+sudo apt install make
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+source "$HOME/.cargo/env"
